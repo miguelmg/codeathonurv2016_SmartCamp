4e40c7ef2341006379b177b5324b73508bae7a73
sfrias <sfrias1@gmail.com>
2016-02-14 16:32:52 +0100

    Corrected error.

---
+++ view/uploader_pre410.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sfrias
+ * Date: 14/02/2016
+ * Time: 16:17
+ */
+// En versiones de PHP anteriores a la 4.1.0, debería utilizarse $HTTP_POST_FILES en lugar
+// de $_FILES.
+
+$dir_subida = '/var/www/uploads/';
+$fichero_subido = $dir_subida . basename($_FILES['fichero_usuario']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
+    echo "El fichero es válido y se subió con éxito.\n";
+} else {
+    echo "¡Posible ataque de subida de ficheros!\n";
+}
+
+echo 'Más información de depuración:';
+print_r($_FILES);
+
+print "</pre>";
